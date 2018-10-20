49a5897ba55b6e09a0daac46c420908d95dd7c0a
Quentin <vFISHv@gmail.com>
2018-10-21 00:12:58 +0800

    m

--- app/src/main/java/net/studymongolian/fontmetrics/MainActivity.java
+++ app/src/main/java/net/studymongolian/fontmetrics/MainActivity.java
@@ -2,7 +2,7 @@ package net.studymongolian.fontmetrics;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
